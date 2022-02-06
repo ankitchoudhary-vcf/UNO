60bef08dd8281abed206fb0a5fbfec58975605dc
ankit <ac2000choudhary@gmail.com>
2022-02-06 17:45:50 +0530

    cards-font added

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import randomCodeGenerator from '../../utils/randomCodeGenerator'
+import './style.css'
+
+const Home = () => {
+    const [roomCode, setRoomCode] = useState('')
+
+    return (
+        <div className='Homepage'>
+            <div className='homepage-menu'>
+                <img src={require('../../assets/logo.png')} width='200px' />
+                <div className='homepage-form'>
+                    <div className='homepage-join'>
+                        <input type='text' placeholder='Game Code' onChange={(event) => setRoomCode(event.target.value)} />
+                        <Link to={`/play?roomCode=${roomCode}`}><button className="game-button green">JOIN GAME</button></Link>
+                    </div>
+                    <h1>OR</h1>
+                    <div className='homepage-create'>
+                        <Link to={`/play?roomCode=${randomCodeGenerator(5)}`}><button className="game-button orange">CREATE GAME</button></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
